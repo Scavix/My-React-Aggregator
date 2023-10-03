ce7e50fa1e1047b3599181ce7fc671a51b719541
Scav <42140533+Scavix@users.noreply.github.com>
2023-10-03 23:08:25 +0200

    Added counter

--- src/LinkList.js
+++ src/LinkList.js
@@ -14,12 +14,17 @@ function LinkList({ links }) {
 
   return (
     <div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
       <input
         type="text"
         placeholder="Search links..."
         value={filter}
         onChange={handleFilterChange}
+        style={{ marginRight: '1%' }}
       />
+        <span>Total Links: {links.length}</span>
+        <span style={{ marginLeft: '10px' }}>Filtered Links: {filteredLinks.length}</span>
+      </div>
       <ul>
         {filteredLinks.map((link, index) => (
           <li key={index}>
